2b6e3e3ec6d57170c8693c22107bcce85927141b
EminliAysun05 <aeminli232@gmail.com>
2024-08-27 00:19:24 +0400

    js lines

--- script.js
+++ script.js
@@ -8,12 +8,20 @@ document.getElementById('add-task-btn').addEventListener('click', function() {
         newTask.innerHTML = `${taskText} <button class="delete-task">Delete</button>`;
         
         taskList.appendChild(newTask);
-        taskInput.value = "";  // Clear the input field
+        taskInput.value = "";  
 
         newTask.querySelector('.delete-task').addEventListener('click', function() {
             newTask.remove();
-        });
+            
+        }
+        
+    );
     } else {
         alert("Please enter a task!");
     }
+});
+document.querySelectorAll('.delete-task').forEach(button => {
+    button.addEventListener('click', function() {
+        this.parentElement.remove();  
+    });
 });
